b8998d0314f4298ddcc4cae29554a2ac6adf7885
S3U-13 <payaknoi003@gmail.com>
2025-09-17 20:39:26 +0700

    Delete QuickAPI directory

--- QuickAPI/config/db.js
+++
@@ -1,9 +0,0 @@
-// connect mysql db for models 
-const { Sequelize } = require("sequelize");
-                                //db name  user name  password
-const sequelize = new Sequelize("my_project", "root", "", {
-  host: "127.0.0.1",
-  dialect: "mysql"
-});
-
-module.exports = sequelize;
--- QuickAPI/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const userController = require("../controllers/userController");
-
-//route
-router.get("/user" , userController.index);
-
-module.exports = router;
