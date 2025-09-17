51eaa7824b790fc0e890f88fd7318db50b2a668f
S3U-13 <payaknoi003@gmail.com>
2025-09-17 15:36:54 +0700

    Delete config directory

--- config/db.js
+++
@@ -1,9 +0,0 @@
-// connect mysql db for models 
-const { Sequelize } = require("sequelize");
-                                //db name  user name  password
-const sequelize = new Sequelize("project_anc", "root", "", {
-  host: "127.0.0.1",
-  dialect: "mysql"
-});
-
-module.exports = sequelize;
