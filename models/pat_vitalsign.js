3259d3d80afb12836af480c16f7dbe57a1d1d03b
S3U-13 <payaknoi003@gmail.com>
2025-11-13 21:28:53 +0700

    version 1.0.9

--- models/pat_vitalsign.js
+++ models/pat_vitalsign.js
@@ -1,31 +1,43 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/pat_connect_db");
 
-const PatVitalSign = sequelize.define("PatVitalSign", {
+const PatVitalSign = sequelize.define(
+  "PatVitalSign",
+  {
     hn: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
     weight: {
-        type: DataTypes.DECIMAL(5, 2), // เช่น 70.55
-        allowNull: true
+      type: DataTypes.DECIMAL(5, 2), // เช่น 70.55
+      allowNull: true,
     },
     height: {
-        type: DataTypes.DECIMAL(5, 2), // เช่น 175.30
-        allowNull: true
+      type: DataTypes.DECIMAL(5, 2), // เช่น 175.30
+      allowNull: true,
     },
     bp_systolic: {
-        type: DataTypes.INTEGER, // ความดันบน
-        allowNull: true
+      type: DataTypes.INTEGER, // ความดันบน
+      allowNull: true,
     },
     bp_diastolic: {
-        type: DataTypes.INTEGER, // ความดันล่าง
-        allowNull: true
+      type: DataTypes.INTEGER, // ความดันล่าง
+      allowNull: true,
     },
-}, {
+    temperature: {
+      type: DataTypes.DECIMAL(5, 2),
+      allowNull: true,
+    },
+    pulse: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+  },
+  {
     tableName: "pat_vitalsign",
-    timestamps: false
-});
+    timestamps: false,
+  }
+);
 
 module.exports = PatVitalSign;
